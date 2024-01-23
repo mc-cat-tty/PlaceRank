3eb7d08d8fbd54606a44d6543a2e5aa2a03dde55
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-01-23 08:44:11 +0100

    Implemented Stemmatizer

--- placerank/preprocessing.py
+++ placerank/preprocessing.py
@@ -3,25 +3,47 @@ This file is a kind of configuration file, where preprocessing strategies are
 defined and one of them can be chosen for the entire project as a default.
 """
 
+import nltk
+import copy
+from nltk.corpus import wordnet
 from whoosh.analysis import *
 from typing import Generator
-import nltk
+from operator import attrgetter
 
 class LemmaFilter(Filter):
     def __init__(self):
         self.__lemmatizerFn = nltk.WordNetLemmatizer().lemmatize
     
-    def __call__(self, tokens: list[Token]) -> Generator[Token, None, None]:
-        tags = nltk.pos_tag([t.text for t in tokens])
+    @staticmethod
+    def to_wordnet_pos(tag: str):
+        """
+        Make treebank POS tags compliant with WordNet POS tags.
+        """
+        if tag.startswith('J'):
+            return wordnet.ADJ
+        elif tag.startswith('V'):
+            return wordnet.VERB
+        elif tag.startswith('N'):
+            return wordnet.NOUN
+        elif tag.startswith('R'):
+            return wordnet.ADV
+        else:
+            return wordnet.NOUN
+    
+    def __call__(self, tokens: Generator[Token, None, None]) -> Generator[Token, None, None]:
+        tokens = list(map(copy.copy, tokens))  # Inefficiently consumes the whole generator to have an integral view over the text field
+        tokens_text = list(map(attrgetter("text"), tokens))
+        tags = list(zip(*nltk.pos_tag(tokens_text)))[1]
+        tags_wn = map(self.to_wordnet_pos, tags)
         
-        for token, tag in zip(tokens, tags):
+        for token, tag in zip(tokens, tags_wn):
             if not token.stopped:
                 token.text = self.__lemmatizerFn(token.text, tag)
             yield token
 
 ANALYZER_NAIVE = RegexTokenizer() | LowercaseFilter() | StopFilter()
 ANALYZER_STEMMER = RegexTokenizer() | LowercaseFilter() | StopFilter() | StemFilter()
-ANALYZER_LEMMATIZER = RegexTokenizer() | LowercaseFilter() | LemmaFilter() | StopFilter()  # Not working, cause tokens is a generator
+ANALYZER_LEMMATIZER = RegexTokenizer() | LowercaseFilter() | LemmaFilter() | StopFilter()
 
 def getDefaultAnalyzer() -> Analyzer:
   return ANALYZER_LEMMATIZER
