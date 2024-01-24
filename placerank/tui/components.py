a21cb24f6a6be735802ed020564da201ac278416
mc-cat-tty <44820563+mc-cat-tty@users.noreply.github.com>
2024-01-24 20:10:00 +0100

    Working on layout. Done basic structure

--- placerank/tui/components.py
+++ placerank/tui/components.py
@@ -21,34 +21,55 @@ class BaseContainer(Overlay):
 
         super().__init__(
             AttrMap(self.top_widget, 'frame'), AttrMap(SolidFill(), 'bg'),
-            align = 'center', width = ('relative', 90),
-            valign = 'middle', height = ('relative', 90),
+            align = 'center', width = ('relative', 95),
+            valign = 'middle', height = ('relative', 95),
             **kwargs
         )
 
 class SearchBar(WidgetWrap):
     def __init__(self, **kwargs):
-        self.text_field = Edit('Textual search: ', wrap='clip')
+        self.label = Text('Textual search: ')
+        self.text_field = Edit(wrap = 'clip')
         self.search_button = Button('Go', on_press = lambda btn: Events.SEARCH.value.notify(self.text_field.edit_text))
-        component = Filler(
+        self.search_bar = Filler(
             Columns((
-                self.text_field,
-                (7, self.search_button)
+                (16, self.label),
+                LineBox(self.text_field, tline='', lline='', rline=''),
+                (6, self.search_button)
             )),
             **kwargs
         )
-        WidgetWrap.__init__(self, component)
+        WidgetWrap.__init__(self, self.search_bar)
     
     def keypress(self, size, key):
         if key != 'enter': return super().keypress(size, key)
         Events.SEARCH.value.notify(self.text_field.edit_text)
 
-class ResultArea(WidgetWrap):
-    def __init__(self):
-        ...
+class SearchArea(WidgetWrap):
+    def __init__(self, **kwargs):
+        self.search_bar = SearchBar()
+        self.result_area = ListBox([Text('Mock')] * 10)
+        self.search_area = Frame(
+            header = self.search_bar,
+            body = self.result_area,
+            focus_part = 'header',
+            **kwargs
+        )
+        WidgetWrap.__init__(self, self.search_area)
 
 class Window(WidgetWrap):
     def __init__(self):
-        self.search_bar = SearchBar(valign='top')
-        self.base_container = BaseContainer(self.search_bar)
+        self.description = Text(
+            '''Full-text search engine for AirBnB listings with support for sentiment analysis and word2vec.\n'''
+            '''Try it yourself with queries like: "Manhattan apartment with amazing skyline view", "Row house nearby Brooklyn Bridge", "Cheap room in dangerous block"'''
+        )
+        self.search_area = SearchArea()
+        self.search_area = LineBox(self.search_area)
+        self.search_area = Padding(self.search_area, width = ('relative', 90), align = 'center')
+        self.inner_container = Frame(
+            header = self.description,
+            body = self.search_area,
+            footer = Text("controls")
+        )
+        self.base_container = BaseContainer(self.inner_container)
         WidgetWrap.__init__(self, self.base_container)
