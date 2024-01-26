50a609fe334b6f167582b3370ff2b47c1ebb8783
giuliocorradini <39673120+giuliocorradini@users.noreply.github.com>
2024-01-26 16:11:25 +0100

    Removed DocumentLogicView that inadvertitely came up

--- placerank/logic_views.py
+++ placerank/logic_views.py
@@ -31,25 +31,6 @@ class InsideAirbnbSchema(Schema):
         """
         return {k:record[k] for k in self.logicview.keys()}
 
-class DocumentLogicView(dict):
-    SCHEMA : Dict[str, FieldType] = {
-        "id": ID(stored = True, unique=True),
-        "name": TEXT(stored = True, field_boost=1.5),
-        "room_type": KEYWORD(stored=True, lowercase=True),
-        "description": TEXT(analyzer=get_default_analyzer()),
-        "neighborhood_overview": TEXT(analyzer=get_default_analyzer())
-    }
-    
-    def __init__(self, record: dict):
-        """
-        Extracts only the required keys from a dictionary representing a dataset record.
-        The required keys are specified in the `SCHEMA` class field.
-        """
-        super().__init__({k:record[k] for k in DocumentLogicView.SCHEMA.keys()})
-    
-    @staticmethod
-    def get_schema() -> Schema:
-        return Schema(**DocumentLogicView.SCHEMA)
 
 class BenchmarkRecordView:
     """
